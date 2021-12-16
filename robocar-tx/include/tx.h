c8b691b4473b40e87fcf2fa526a1241fadb1d4de
grantmoe <grant@grantmoe.com>
2021-12-16 15:53:54 -0800

    Add tx.h to hold defines

---
+++ robocar-tx/include/tx.h
@@ -0,0 +1,31 @@
+#ifndef TX_H
+#define TX_H
+
+#define CH_3_PIN 30
+#define CH_4_PIN 11
+#define ST_PIN 2
+#define ST_EXP_PIN 3
+#define ST_REV_PIN 16
+#define ST_TRM_PIN 4
+#define TH_PIN 28
+#define TH_EXP_PIN 29
+#define TH_REV_PIN 27
+#define TH_TRM_PIN 5
+#define TFT_DC_PIN 15
+#define TFT_CS_PIN 7
+#define TFT_RST_PIN -1
+
+#define DRIVE_MODE 0
+#define CALIBRATE_MODE 1
+#define PAUSE_MODE 2
+
+#define MAX_IN 1023
+#define MAX_OUT 127
+#define MIN_IN 0
+#define MIN_OUT -127
+#define NEUTRAL_IN 512
+#define NEUTRAL_OUT 0
+#define SLIDER_HIGH 682
+#define SLIDER_LOW 341
+
+#endif
